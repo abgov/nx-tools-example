ba15ca7f9f51bb9ffcc77bcea3fdafda2b3c0fd7
tingzuge-dio <47162374+tingzuge-dio@users.noreply.github.com>
2021-04-14 11:16:10 -0600

    feat: checking in a mern example solution

---
+++ apps/mern-example-service/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'mern-example-service',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/mern-example-service',
+};
